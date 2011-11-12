d59deb8d0a4b7ea473f3cebe852889f78bd47b55
Mark van Seventer <mark@vseventer.com>
2011-11-12 21:39:17 +0100

    After passing Basic - Text tests

---
+++ src/lady.js
@@ -0,0 +1,227 @@
+function Lady() {
+
+	/**
+	 * Native doucment.write
+	 * @access private
+	 * @var function _write
+	 */
+	this._write = document.write;
+
+	/**
+	 * Autocapture flag
+	 * @access public
+	 * @var boolean autoCapture
+	 */
+	this.autoCapture = true;
+
+	/**
+	 * Queue
+	 * @access public
+	 * @var Array queue
+	 */
+	this.queue = [];
+
+	// Constructor
+	this.init();
+}
+
+/**
+ * Enqueues item
+ * 
+ * @access public
+ * @param String id
+ * @param String data (optional)
+ * @returns String
+ */
+Lady.prototype.enqueue = function(id, data) {
+	data = data || null;
+	if(null === data) {//generate ID
+		data = id;
+		id   = this._id();
+	}
+
+	this.queue.push({id: id, data: data});
+	return id;
+};
+
+/**
+ * Initializes lady
+ * 
+ * @access public
+ * @returns void
+ */
+Lady.prototype.init = function() {
+	// Overwrite native document.write
+	if(this.autoCapture) {
+		document.write = (function(that) {
+			return function(raw) {
+				var id = that.enqueue(raw);
+				that._write.call(this, '<span id="' + id + '"></span>');
+			};
+		}(this));
+	}
+};
+
+/**
+ * Renders queue
+ * 
+ * @access public
+ * @returns integer (number of rendered items)
+ */
+Lady.prototype.render = function() {
+	var item,
+	    result = 0,
+	    target;
+
+	// Dequeue items
+	while(0 < this.queue.length) {
+		item   = this.queue.shift();
+		target = document.getElementById(item.id) || null;
+		if(null !== target) {//target found
+			this._render(item.data, target);
+			result += 1;
+		}
+	}
+	return result;
+};
+
+/**
+ * Shallow clones node
+ * 
+ * @access private
+ * @param HTMLElement node
+ * @returns HTMLElement
+ */
+Lady.prototype._clone = function(node) {
+	if(document.importNode) {
+		return node.cloneNode(false);
+	}
+
+	// Internet Explorer
+	if('#text' === node.nodeName) {//simple node
+		return document.createTextNode(node.nodeValue);
+	}
+	
+	// Complex node
+	var i,
+	    newNode = document.createElement(node.nodeName);
+
+	// Add attributes
+	for(i = 0; i < node.attributes.length; i += 1) {
+		newNode.setAttribute(
+			node.attributes[i].name,
+			node.attributes[i].value
+		);
+	}
+	return newNode;
+};
+
+/**
+ * Evaluates string
+ * 
+ * @access private
+ * @param String data
+ * @returns void
+ */
+Lady.prototype._eval = function(data) {
+	// @link http://perfectionkills.com/global-eval-what-are-the-options
+	// @link http://www.blog.highub.com/javascript/decoding-jquery-evaluates-a-script-in-a-global-context/
+	(window.execScript || function(data) {
+		window['eval'].call(window, data);
+	})(data);
+};
+
+/**
+ * Injects node into target
+ * 
+ * @param DOMElement node
+ * @param DOMElement target
+ * @returns void
+ */
+Lady.prototype._inject = function(node, target) {
+	var capture = '',
+	    doc,
+	    i,
+	    newNode = this._clone(node),
+	    tmp;
+
+	// Capture document.writes from node
+	document.write = (function() {
+		return function(raw) {
+			capture += raw;
+		};
+	}());
+
+	// Handle node
+	if('#text' === node.nodeName && 'script' === node.parentNode.nodeName.toLowerCase()) {
+		this._eval(node.nodeValue);
+
+		// Disconnect obsolete parent
+		tmp = target.parentNode;
+		tmp.removeChild(target);
+		target = tmp;
+	}
+	else {//inject
+		target.appendChild(newNode);
+	}
+
+	// Inject capture
+	if('' !== capture) {
+		this._render(capture, target);
+	}
+
+	// Inject children
+	for(i = 0; i < node.childNodes.length; i += 1) {
+		this._inject(node.childNodes[i], newNode);
+	}
+};
+
+/**
+ * Converts string to DOM
+ * 
+ * @access private
+ * @param String str
+ * @returns Array (list of nodes)
+ */
+Lady.prototype._stringToDom = function(str) {
+	str = '<div>' + str + '</div>';//force single root
+
+	// Create document
+	// @link https://gist.github.com/905833
+	var doc;
+	if(window.DOMParser) {
+		doc = new DOMParser().parseFromString(str, 'application/xhtml+xml');
+	}
+	else {//Internet Explorer
+		doc = new ActiveXObject('Microsoft.XMLDOM');
+		doc.loadXML(str);
+	}
+	return doc.documentElement.childNodes;//exclude root
+};
+
+/**
+ * Generates ID
+ * 
+ * @access private
+ * @returns String
+ */
+Lady.prototype._id = function() {
+	return '__' + new Date().getTime();
+};
+
+/**
+ * Renders data into target
+ * 
+ * @param String data
+ * @param HTMLElement target
+ * @returns void
+ */
+Lady.prototype._render = function(data, target) {
+	var doc = this._stringToDom(data),
+	    i;
+
+	// Evaluate nodes
+	for(i = 0; i < doc.length; i += 1) {
+		this._inject(doc[i], target);
+	}
+};
--- tests/lady-test.js
+++ tests/lady-test.js
@@ -1,2 +1,30 @@
-module('Nodes');
+module('Nodes', {
+	setup: function() {
+		this.lady   = new Lady();
+		this.target = document.getElementById('qunit-fixture');
+	}
+});
+
+test('Basic - Text', function() {
+	this.lady.enqueue(this.target.id, mock(''));
+	this.lady.render();
+	equal('', this.target.innerHTML, 'Empty textnode');
+
+	this.lady.enqueue(this.target.id, mock('foo'));
+	this.lady.render();
+	equal('foo', this.target.innerHTML, 'Textnode');
+});
+
 module('Scripts');
+
+/**
+ * Mocks input
+ * NOTE necessary since we can’t use document.write directly
+ * 
+ * @param String input
+ * @return String
+ */
+function mock(input) {
+	// @link http://phpjs.org/functions/addslashes:303
+	return "<script>document.write('" + input.replace(/[\\']/g, '\\$&') + "');<\/script>";
+}
